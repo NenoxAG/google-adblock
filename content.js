dbcc6c2d116fd1a6f8f3416620ee41d72c82d133
Nenox <nenox@nenox.dev>
2021-03-03 16:06:27 +0100

    Initial commit

---
+++ content.js
@@ -0,0 +1,4 @@
+document.getElementById("tads")?.remove();
+document.getElementById("bottomads")?.remove();
+document.getElementsByClassName("commercial-unit-desktop-rhs")[0]?.remove();
+document.getElementsByClassName("commercial-unit-desktop-top")[0]?.remove();
